9db0b4fb2c3e5792a9a3ecb971c3af0845c68ed9
mszczykowski <67839881+mszczykowski@users.noreply.github.com>
2021-03-23 20:38:32 +0100

    initial commit

---
+++ PS2/Forms/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PS2.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Ulica")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztowy")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto")]
+        [Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string City { get; set; }
+        [Display(Name = "Numer")]
+        public int Number { get; set; }
+    }
+}
