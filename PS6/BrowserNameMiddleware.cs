793c2a466455b4f6ff978a7a5dac77b229d024ca
mszczykowski <67839881+mszczykowski@users.noreply.github.com>
2021-04-20 13:27:27 +0200

    Initial commit

---
+++ PS6/BrowserNameMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shyjus.BrowserDetection;
+
+namespace PS6
+{
+    public class BrowserNameMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserNameMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext, IBrowserDetector browserDetector)
+        {
+            var browser = browserDetector.Browser;
+            if (browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.EdgeChromium || browser.Name == BrowserNames.InternetExplorer)
+            {
+                httpContext.Response.WriteAsync("<b>Przegladarka nie jest obslugiwana</b>");
+            }
+            return _next(httpContext);
+        }
+    }
+}
---
+++ PS6/BrowserNameMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS6
+{
+    public static class BrowserNameMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowserNameMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserNameMiddleware>();
+        }
+    }
+}
